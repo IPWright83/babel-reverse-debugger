40dc7608e91a3a68eaadd9b371cdfcfc84fce534
Ian Wright <ian@geckoboard.com>
2024-07-05 09:31:33 +0100

    Fix loop tests

--- src/updates.js
+++ src/updates.js
@@ -10,19 +10,19 @@ function inject({ t, path, ASTType }) {
     const name = utils.extractName(path) ?? argument.name;
     const lineNumber = utils.getLineNumber(path);
 
-    const displayValue = t.numericLiteral(1);
-    const newValue = operator === "=" ? argument : t.binaryExpression(
+    const value = t.numericLiteral(1);
+    const displayValue = operator === "=" ? argument : t.binaryExpression(
         operator === "++" ? "+" : "-",
         argument,
-        displayValue
+        value
     );
 
     const captureAssignmentCall = t.callExpression(t.identifier("___captureAssignment"), [
         t.stringLiteral(ASTType),
         t.stringLiteral(name),
         t.numericLiteral(lineNumber),
-        newValue,
-        newValue,
+        displayValue,
+        value,
     ]);
 
     // Replace the original update expression with a compound assignment
